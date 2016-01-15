238bf96394a31db69422259b5f5d3c07f01cce68
Alan Rhode <alanmichaelrhode@gmail.com>
2016-01-15 14:01:24 -0500

    state machine

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,4 +3,20 @@ class Mission < ActiveRecord::Base
 	belongs_to :vehicle
 	validates_presence_of :cargo
 	validates_presence_of :cost
+
+	TYPES = ["Philadelphia", "Atlantic City", "New York", "Brooklyn", "Chicago", "New Orleans"]
+
+	def show_me_origin
+
+		TYPES[self.origin]
+
+	end
+
+	def show_me_destination
+
+		TYPES[self.destination]
+
+	end
+
+
 end
